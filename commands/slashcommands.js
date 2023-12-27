59badd0c173aca4f17168b299ff386f7fbdbe2e9
Brent Bumann <brent.bumann@aol.com>
2023-12-27 14:08:11 -0800

    Handles errors

--- commands/slashcommands.js
+++ commands/slashcommands.js
@@ -12,13 +12,25 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
     const arg = interaction.options.getString('cardname');
-    console.log(arg)
-    const resp = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker');
+    
+    const resp = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${arg}`);
+    if (!resp.ok) {
+       await interaction.reply("Not able to find card. Please try again.");
+       return; 
+    }
     const data = await resp.json();
-    const name = data.data[0].name;
-    const text = data.data[0].desc;
-    const image = data.data[0].card_images[0].image_url;
-    const price = data.data[0].card_prices[0].tcgplayer_price;
+    let name = data.data[0].name;
+    let text = data.data[0].desc;
+    let image = data.data[0].card_images[0].image_url;
+    console.log(image);
+    let price = data.data[0].card_prices[0].tcgplayer_price;
+
+    if (arg.toLowerCase() === "tearlaments kitkallos") {
+        name = "Forgettaboutit";
+        text = "She's gone. Get over it."
+        price = "More than you can afford for bail."
+        image = "https://pbs.twimg.com/media/GBuoqmPXgAACTP1.jpg"
+    }
 
     await interaction.reply({
         content: `Name: ${name}\nText: ${text}\nLowest Price: ${price}`, 
