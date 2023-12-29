5c8302088c92b15e5b24dcc90466d9aef97e2819
Brent Bumann <brent.bumann@aol.com>
2023-12-29 11:26:14 -0800

    Add 1 min timer for showing cards

--- bot.js
+++ bot.js
@@ -11,13 +11,19 @@ const client = new Client({
 async function handleInteraction(interaction) {
     if (!interaction.isCommand()) return;
 	if (interaction.commandName === "cardlookup") {
-		await commands.execute(interaction);
+		await commands.execute(interaction, client);
 	}
 
 }
 
 client.once(Events.ClientReady, () => {
-    console.log('YGO Client is reporting for duty');
+	let bot;
+	if (process.env.CLIENTID === 1185500795781795880) {
+		bot = 'YGO Client';
+	} else {
+		bot = 'YGO Client (test)'
+	}
+    console.log(`${bot} is reporting for duty`);
 });
 
 client.login(process.env.TOKEN);
--- commands/slashcommands.js
+++ commands/slashcommands.js
@@ -10,9 +10,9 @@ export const data = new SlashCommandBuilder()
 		.setRequired(true)
     );
 
-export async function execute(interaction) {
+export async function execute(interaction, client) {
     const arg = interaction.options.getString('cardname');
-    
+
     const resp = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${arg}`);
     if (!resp.ok) {
        await interaction.reply("Not able to find card. Please try again.");
@@ -22,7 +22,6 @@ export async function execute(interaction) {
     let name = data.data[0].name;
     let text = data.data[0].desc;
     let image = data.data[0].card_images[0].image_url;
-    console.log(image);
     let price = data.data[0].card_prices[0].tcgplayer_price;
 
     if (arg.toLowerCase() === "tearlaments kitkallos") {
@@ -32,7 +31,19 @@ export async function execute(interaction) {
         image = "https://pbs.twimg.com/media/GBuoqmPXgAACTP1.jpg"
     }
 
-    await interaction.reply({
+    const message = await interaction.reply({
         content: `Name: ${name}\nText: ${text}\nLowest Price: ${price}`, 
         files: [{attachment: `${image}`}]});
+
+
+    setTimeout(async () => {
+        const channel = await client.channels.cache.get(interaction.channelId);
+        try {
+            await message.delete();
+        } catch (error) {
+            await channel.send("Issue deleting message after timeframe");
+            return;
+        }
+        await channel.send(`${interaction.user.username} requested a look up for ${arg}`);
+    }, 60000)
 }
